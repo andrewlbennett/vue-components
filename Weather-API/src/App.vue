1aef884abfc9a11ed5586d25bbc6203fd0dc1a4f
Andrew Bennett <hello@andrewbennett.us>
2018-03-01 19:49:34 -0600

    Added Wunderground API

---
+++ Weather-API/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <p> {{ results.current_observation.temp_f }} </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+const url = 'http://api.wunderground.com/api/API_KEY_GOES_HERE/conditions/q/IL/Chicago.json'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            results: []
+        }
+    },
+    mounted() {
+        axios.get(url).then(response => {
+            this.results = response.data
+        })
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
