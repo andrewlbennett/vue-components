8e9de38391f2a9fb9f093ff86e60463c1a7e3d9b
Andrew Bennett <hello@andrewbennett.us>
2018-03-10 17:13:01 -0600

    Added Chartsjs example

---
+++ Chartjs/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="app">
+        <div class="graph">
+            <canvas id="planet-chart"></canvas>
+        </div>
+    </div>
+</template>
+
+<script>
+import Chart from 'chart.js';
+import planetChartData from './chart-data.js';
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            planetChartData: planetChartData,
+        }
+    },
+    methods: {
+        createChart(chartId, chartData) {
+            const ctx = document.getElementById(chartId);
+            const myChart = new Chart(ctx, {
+                type: chartData.type,
+                data: chartData.data,
+                options: chartData.options,
+            });
+        }
+    },
+    mounted() {
+        this.createChart('planet-chart', this.planetChartData);
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+
+.graph {
+    width: 800px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
